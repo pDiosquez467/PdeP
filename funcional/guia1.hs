c3332110718bc047298148e4d65365777602f015
pdiosquez467 <pablodiosquez467@gmail.com>
2025-07-25 18:34:25 -0300

    Agrego resolución ejercicio 1.2 - esMultDe

--- funcional/guia1.hs
+++ funcional/guia1.hs
@@ -9,3 +9,18 @@
 -- False
 esMultiploDeTres :: Integer -> Bool
 esMultiploDeTres x = x `mod` 3 == 0
+
+
+-- | Devuelve 'True' si el segundo número es múltiplo del primero, y 'False' en caso contrario.
+--
+-- El primer parámetro representa el divisor, y el segundo, el número que se evalúa.
+--
+-- === Ejemplos
+--
+-- >>> esMultiploDe 2 16
+-- True
+--
+-- >>> esMultiploDe 5 16
+-- False
+esMultiploDe :: Integer -> Integer -> Bool 
+esMultiploDe divisor numero = numero `mod` divisor == 0
