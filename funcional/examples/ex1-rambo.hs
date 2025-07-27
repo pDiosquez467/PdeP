bf72808ba104ca3c4a1b8930a975702c498f436e
pdiosquez467 <pablodiosquez467@gmail.com>
2025-07-27 10:44:51 -0300

    Agrego enunciado Ejemplo Rambo

---
+++ funcional/examples/ex1-rambo.hs
@@ -0,0 +1,6 @@
+-- "Rambo tiene dos armas, una principal y otra secundaria.""
+-- "Cada una cuenta con un cargador de cierta capacidad con alguna cantidad de balas."
+
+-- 1. Averiguar cuántas balas le quedan a Rambo en total.
+-- 2. Dada un arma, disparar si tiene balas.
+-- 3. Hacer que Rambo dispare todo a la vez.
