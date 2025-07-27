e8a9567ab48a8c31c1b1a2eedf0d0253af2f5e37
pdiosquez467 <pablodiosquez467@gmail.com>
2025-07-26 21:31:03 -0300

    Agrego resolución Ejercicio 1.10 - Dispersión

--- funcional/guia1.hs
+++ funcional/guia1.hs
@@ -123,3 +123,88 @@ haceFrio gradosFah = fahrToCelsius gradosFah < 8
 mcm :: Integer -> Integer -> Integer
 mcm numero otroNumero = (numero * otroNumero) `div` gcd numero otroNumero
   
+-- === Ejercicio 10
+
+type Medicion = Integer
+
+-- | Indica si la dispersión de las mediciones recibidas es chica.
+--
+-- Se considera que la dispersion es chica cuando es menor a 30cm.
+-- === Ejemplos
+--
+-- >>> diasParejos 233 123 344
+-- False 
+--
+-- >>> diasParejos 233 222 234
+-- True 
+diasParejos :: Medicion -> Medicion -> Medicion -> Bool
+diasParejos m1 m2 m3 = dispersion m1 m2 m3 < 30
+
+
+-- | Indica si la dispersión de las mediciones recibidas es grande.
+--
+-- Se considera que la dispersion es grande cuando es mayor a 100cm.
+-- === Ejemplos
+--
+-- >>> diasLocos 233 123 344
+-- True 
+--
+-- >>> diasLocos 233 222 234
+-- False  
+diasLocos :: Medicion -> Medicion -> Medicion -> Bool
+diasLocos m1 m2 m3 = dispersion m1 m2 m3 > 100 
+
+
+-- | Indica si la dispersión de las mediciones recibidas no es ni chica ni grande.
+--
+-- Se considera que la dispersion es chica cuando está entre 30cm y 100cm inclusive.
+-- === Ejemplos
+--
+-- >>> diasNormales 233 123 344
+-- False 
+--
+-- >>> diasNormales 233 230 201
+-- True 
+diasNormales :: Medicion -> Medicion -> Medicion -> Bool
+diasNormales m1 m2 m3 = not (diasParejos m1 m2 m3) && not (diasLocos m1 m2 m3)
+
+
+-- | Devuelve la diferencia entre la más alta y la más baja de las mediciones dadas.
+--
+-- === Ejemplos
+--
+-- >>> dispersion 12 34 5
+-- 29
+--
+-- >>> dispersion 100 34 71
+-- 66
+dispersion :: Medicion -> Medicion -> Medicion -> Integer
+dispersion m1 m2 m3 = max3 m1 m2 m3 - min3 m1 m2 m3 
+
+
+-- Funciones auxiliares para max y min de tres elementos
+
+-- | Devuelve el máximo entre los tres números dados.
+--
+-- === Ejemplos
+--
+-- >>> max3 12 3 4
+-- 12
+--
+-- >>> max3 (-1) 3 7
+-- 7
+max3 :: Integer -> Integer -> Integer -> Integer
+max3 x y z = x `max` y `max` z 
+
+
+-- | Devuelve el mínimo entre los tres números dados.
+--
+-- === Ejemplos
+--
+-- >>> min3 12 3 4
+-- 3
+--
+-- >>> min3 (-1) 3 7
+-- -1
+min3 :: Integer -> Integer -> Integer -> Integer
+min3 x y z = x `min` y `min` z 
