608f0b9cd89387fdcfff84d43ddeedccc954cd92
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-28 14:18:58 -0300

    Agrego resolución Ejercicio 4.4 - Horarios

--- funcional/guias/guia4.hs
+++ funcional/guias/guia4.hs
@@ -1,4 +1,4 @@
--- Ejercicio 1
+-- === Ejercicio 1
 
 -- | Devuelve la suma de los elementos de la lista.
 --
@@ -13,7 +13,7 @@ suma :: Num a => [a] -> a
 suma [] = 0
 suma (x : xs) = x + suma xs 
 
--- Ejercicio 3 
+-- === Ejercicio 3 
 
 -- | Devuelve, dada una lista de listas,  si la concatenación de las sublistas es una lista capicua.
 --
@@ -23,3 +23,56 @@ suma (x : xs) = x + suma xs
 -- True 
 esCapicua :: (Eq a, Foldable t) => t [a] -> Bool
 esCapicua silabas = ((reverse . concat) silabas) == concat silabas 
+
+-- === Ejercicio 4 
+
+
+-- | Dada una función 'f' y una tupla de horarios con sus registros,
+-- devuelve el nombre del horario (como 'String') en el cual la función
+-- aplicada a la lista de minutos (o llamadas) es mayor o igual.
+--
+-- === Ejemplo
+--
+-- >>> obtenerInfoHorarios suma (("reducido",[10,20]), ("normal",[5,6,8]))
+-- "reducido"
+obtenerInfoHorarios :: ([Int] -> Int) -> ((String, [Int]), (String, [Int])) -> String
+obtenerInfoHorarios f (horario1, horario2)
+  | f (snd horario1) >= f (snd horario2) = fst horario1
+  | otherwise                            = fst horario2
+
+
+
+-- | Devuelve el nombre del horario donde se habló más minutos.
+--
+-- === Ejemplo
+--
+-- >>> cuandoHabloMasMinutos (("reducido",[20,10,25,15]),("normal",[10,5,8,2,9,10]))
+-- "reducido"
+cuandoHabloMasMinutos :: ((String, [Int]), (String, [Int])) -> String
+cuandoHabloMasMinutos = obtenerInfoHorarios suma
+
+
+
+-- | Devuelve el nombre del horario donde se hicieron más llamadas.
+--
+-- === Ejemplo
+--
+-- >>> cuandoHizoMasLlamadas (("reducido",[20,10,25,15]),("normal",[10,5,8,2,9,10]))
+-- "normal"
+cuandoHizoMasLlamadas :: ((String, [Int]), (String, [Int])) -> String
+cuandoHizoMasLlamadas = obtenerInfoHorarios longitud
+
+
+
+-- | Devuelve la cantidad de elementos de una lista.
+--
+-- === Ejemplos
+--
+-- >>> longitud ["a", "hi!", "Ozzy"]
+-- 3
+--
+-- >>> longitud []
+-- 0
+longitud :: [a] -> Int
+longitud []     = 0
+longitud (_:xs) = 1 + longitud xs
