e6b93ae359c83c8d51b3e16c4163381d77e32399
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-07-28 10:38:37 -0300

    Agrego resolución Ejercicio 3 - cuentaBizarra. Agrego docs de todas las funciones

--- funcional/guias/guia3.hs
+++ funcional/guias/guia3.hs
@@ -1,17 +1,38 @@
--- fst3, snd3, trd3
-
+-- | Devuelve el primer elemento de una tupla de tres componentes.
+--
+-- === Ejemplos
+--
+-- >>> fst3 (1, 'a', True)
+-- 1
 fst3 :: (a, b, c) -> a 
 fst3 (x, _, _) = x 
 
+
+-- | Devuelve el segundo elemento de una tupla de tres componentes.
+--
+-- === Ejemplos
+--
+-- >>> snd3 (1, 'a', True)
+-- 'a'
 snd3 :: (a, b, c) -> b 
 snd3 (_, x, _) = x 
 
+
+-- | Devuelve el tercer elemento de una tupla de tres componentes.
+--
+-- === Ejemplos
+--
+-- >>> trd3 (1, 'a', True)
+-- True
 trd3 :: (a, b, c) -> c 
 trd3 (_, _, x) = x 
 
--- | Devuelve una tupla con el resultado de aplicar las funciones dadas al elemento dado.
+
+
+-- | Devuelve una tupla con el resultado de aplicar dos funciones distintas al mismo valor.
 --
 -- === Ejemplos
+--
 -- >>> aplicar (doble, triple) 8
 -- (16, 24)
 --
@@ -20,3 +41,25 @@ trd3 (_, _, x) = x
 aplicar :: (a -> b, a -> c) -> a -> (b, c)
 aplicar (f, g) x = (f x, g x)
 
+
+-- | Devuelve un número a partir de una tupla de dos enteros, de acuerdo a estas reglas:
+-- 
+-- - Si el primer número es mayor que el segundo, devuelve la suma.
+-- - Si el segundo número excede al primero por más de 10, devuelve la resta.
+-- - En cualquier otro caso, devuelve el producto.
+--
+-- === Ejemplos
+--
+-- >>> cuentaBizarra (10, 5)
+-- 15
+--
+-- >>> cuentaBizarra (5, 20)
+-- 15
+--
+-- >>> cuentaBizarra (4, 10)
+-- 40
+cuentaBizarra :: (Integral a) => (a, a) -> a
+cuentaBizarra (num1, num2)
+                        | num1 > num2      = num1 + num2
+                        | num2 - num1 > 10 = num2 - num1
+                        | otherwise        = num1 * num2
