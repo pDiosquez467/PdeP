518c3df4342c9fdd48f007efc690d6cd2a9bb261
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-08-02 21:29:45 -0300

    Agrego corrección de tipos en punto 2

--- funcional/guias/examenes/gimnasio.hs
+++ funcional/guias/examenes/gimnasio.hs
@@ -5,6 +5,7 @@
 type Peso = Int 
 type Tiempo = Int 
 type Grados = Int 
+type Velocidad = Int 
 type Calorias = Int 
 
 data Gimnasta = Gimnasta {
@@ -19,7 +20,7 @@ data Rutina = Rutina {
     ejercicios :: [Ejercicio]
 }
 
-type Ejercicio = Gimnasta -> Gimnasta
+type Ejercicio = Tiempo -> (Gimnasta -> Gimnasta)
 
 -----------------
 -- === Ejercicios 
@@ -30,3 +31,35 @@ tonificar delta gimnasta = gimnasta { tonificacion = tonificacion gimnasta + del
 
 quemarCalorias :: Calorias -> Gimnasta -> Gimnasta
 quemarCalorias cal gimnasta = gimnasta { peso = peso gimnasta - cal `div` 500 }
+
+-- 2.
+
+cinta :: Velocidad -> Ejercicio
+cinta velocidad tiempo = quemarCalorias (10 * velocidad * tiempo) 
+
+caminata :: Ejercicio
+caminata = cinta 5  
+
+pique :: Ejercicio
+pique tiempo = cinta ((20 + tiempo) `div` tiempo) tiempo
+
+pesas :: Peso -> Ejercicio
+pesas peso tiempo | tiempo < 10 = id  
+                  | otherwise   = tonificar peso 
+
+colina :: Grados -> Ejercicio
+colina inclinacion tiempo = quemarCalorias (2 * tiempo * inclinacion) 
+
+montaña :: Grados -> Ejercicio
+montaña inclinacion tiempo = 
+    let tiempoAsignado = tiempo `div` 2
+    in tonificar 3 . colina (inclinacion + 5) tiempoAsignado . colina inclinacion tiempoAsignado
+
+-- 3. Implementar una función realizarRutina, que dada una rutina y un gimnasta retorna el 
+-- gimnasta resultante de realizar todos los ejercicios de la rutina, repartiendo el tiempo
+-- total de la rutina en partes iguales. Mostrar un ejemplo de uso con una rutina que incluya
+-- todos los ejercicios del punto anterior.
+
+realizarRutina :: Rutina -> (Gimnasta -> Gimnasta)
+realizarRutina rutina gimnasta =  
+    foldr (\ ejercicio gimnasta -> ejercicio (duracionTotal rutina `div` length (ejercicios rutina)) gimnasta ) gimnasta (ejercicios rutina)
